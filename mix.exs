a6da24bfff4a4676428f2e12adb6e44446490837
thread <thread@threadbox.net>
2021-11-29 10:36:09 -0500

    bumped version, changelog, deps

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Versioned.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/instinctscience/versioned"
-  @version "0.2.1"
+  @version "0.3.0"
 
   def project do
     [
